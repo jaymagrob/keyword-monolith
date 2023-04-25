35b53472f3a5a1116f8d6b61c5128307b31d4be9
John Robertson <johnmagnusrobertson@gmail.com>
2023-04-25 18:08:29 +0100

    DEV-alpha, fix deisgn v2

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,35 +8,48 @@ export default function Home() {
 
   return (
     <>
-      <h1>Hello</h1>
-      <input
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          setIsLoading(true)
-          fetch(`/api/google?keyword=${keyword}&country=us&language=en`, {
-            method: 'GET',
-            headers: {
-              // "Content-Type": "application/json",
-            },
-          })
-            .then(i => i.json())
-            .then((i) => {
-              setData(i)
-              console.log(i)
-            })
-            .finally(() => setIsLoading(false))
-        }
-        }
-      >click</button>
+      <header className="under-border">John&apos;s Keyword Tool</header>
+      <nav>
+        <div>
+          <h1 className="underline">Generate keyword ideas</h1>
+          <p>Try entering a keyword or phrase related to your business or industry and explore the insights that the site provides. By understanding the questions and concerns of your audience, you can create content that is more relevant, valuable, and engaging. So go ahead and give it a try – you might just discover new opportunities to connect with your audience and drive more traffic to your website.</p>
+        </div>
+        <div>
+          <form onSubmit={e => e.preventDefault()}>
+            <input
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+            />
+            <button
+              type="submit"
+              onClick={() => {
+                setIsLoading(true)
+                fetch(`/api/google?keyword=${keyword}&country=us&language=en`, {
+                  method: 'GET',
+                  headers: {
+                    // "Content-Type": "application/json",
+                  },
+                })
+                  .then(i => i.json())
+                  .then((i) => {
+                    setData(i)
+                    console.log(i)
+                  })
+                  .finally(() => setIsLoading(false))
+              }
+              }
+            >click</button>
+          </form>
+        </div>
+      </nav>
       {isLoading && <h2>Is Loading....</h2>}
-      {data && (
-        <main>
-          <Data data={data as DataType}/>
-        </main>
-      )}
+      {
+        data && (
+          <main>
+            <Data data={data as DataType} />
+          </main>
+        )
+      }
     </>
   )
 }
